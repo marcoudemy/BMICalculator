21535f8a4e0504724177f0640a2712b915712202
marcoudemy <fittizio22@gmail.com>
2025-07-03 19:01:32 +0200

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("BMI Calculator");
 
-        int heightCm = 175;
-        int weightKg = 70;
+        // initialize the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("insert heigh in cm:");
+        int heightCm = scanner.nextInt();
+        System.out.println("insert weight in kg:");
+        int weightKg = scanner.nextInt();
+
+        // closes the scanner to release resources
+        scanner.close();
 
         // convert the height from cm to meters
         double heightMeters = heightCm / 100.0;
